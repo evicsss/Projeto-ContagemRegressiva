dcc7ecbc8ef27c97cf6d50a211cf472713abaf04
Évila Oliveira <evilavictoria@hotmail.com>
2025-03-19 19:10:37 -0300

    atualizando data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { calculateTimeLeft, render } from "./countdown.js";
 
-const targetDate = new Date(2025, 5, 9, 21, 35);
+const targetDate = new Date(2025, 4, 9, 21, 35);
 
 const countdown = () => {
     const timeLeftObj = calculateTimeLeft(targetDate);
